3f49298c66db9cf9b92cedcc01908d757118ab61
Oyel Daniel <102643280+OyelDaniel@users.noreply.github.com>
2022-10-26 00:06:04 +0800

    Add files via upload

---
+++ Multiplication Table v2 180112.cpp
@@ -0,0 +1,22 @@
+#include "stdio.h"
+int main ()
+{
+	for(int i=1; i<13; i++)
+    {
+      printf("\nsifir %d\n", i);
+      printf("1 * %d = %d\n",i,i*1);
+      printf("2 * %d = %d\n",i,i*2);
+      printf("3 * %d = %d\n",i,i*3);
+      printf("4 * %d = %d\n",i,i*4);
+      printf("5 * %d = %d\n",i,i*5);
+      printf("6 * %d = %d\n",i,i*6);
+      printf("7 * %d = %d\n",i,i*7);
+      printf("8 * %d = %d\n",i,i*8);
+      printf("9 * %d = %d\n",i,i*9);
+      printf("10 * %d = %d\n",i,i*10);
+      printf("11 * %d = %d\n",i,i*11);
+      printf("12 * %d = %d\n\n",i,i*12);
+	}
+	return 0;
+}
+
